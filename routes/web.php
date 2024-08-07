c85fb7e1cf6e109048c265b6667be8c290806328
henleyechivarre1 <henley@bpoc.co.jp>
2024-08-07 17:00:37 +0800

    test page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TestController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +23,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::group([
+        'prefix' => 'test',
+        'as' => 'test.',
+    ], function () {
+        Route::get('/', [TestController::class, 'index'])->name('index');
+        Route::get('/create', [TestController::class, 'create'])->name('create'); //not yet implemented
+        Route::patch('/store', [TestController::class, 'store'])->name('store'); //not yet implemented
+    });
+
 });
 
 require __DIR__.'/auth.php';
